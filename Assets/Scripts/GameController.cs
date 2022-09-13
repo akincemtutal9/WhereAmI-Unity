f4b1cab4f0ef72f13d5919fc4bef9e1871959c2e
akincemtutal9 <akintutall@gmail.com>
2022-09-13 13:45:36 +0300

    Deleting Place List
    Core mechanics of the game has ended. Now we can choose our places according to shuffled list.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,18 +9,21 @@ using Random = UnityEngine.Random;
 
 public class GameController : MonoBehaviour
 {
+    public string[] yerler = { };// ilk basta oyuna yuklenen yerler
 
+    [SerializeField] private List<string> yerListesi = null;// Yerleri listeye ekle
 
-    public string[] yerler = { };
-
-    [SerializeField] private List<string> yerListesi = null;
-
+    [SerializeField] private string[] dataYerleri;// Listeyi Arraye çevir
 
     public Text placeText;
     public Text playerCountText;
     
     public int oyuncuSayisi;
-    public int yerlerdekiPlacelerdenBiri;
+    
+    public int yerlerdekiPlacelerdenBiri;// ilk array
+    public int yerListesindekiPlacelerdenbiri;// ikinci array
+
+
 
 
     
@@ -27,17 +31,22 @@ public class GameController : MonoBehaviour
     {
         oyuncuSayisi = 0;
         yerlerdekiPlacelerdenBiri = Random.Range(0, yerler.Length);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        dataYerleri = yerListesi.ToArray();
     }
 
     public void TexteYeriYaz()
     {
-        placeText.text = yerler[yerlerdekiPlacelerdenBiri];   
+        placeText.text = dataYerleri[0];
+
+        yerListesi.RemoveAt(0);
+
+       
     }
 
     public void TexttekiYeriBosalt()
@@ -47,6 +56,7 @@ public class GameController : MonoBehaviour
     public void OyuncuSayisiniArttir()
     {
         oyuncuSayisi++;
+        Shuffle(yerListesi);
     }
     public void GetOyuncuSayisi()
     {
@@ -66,4 +76,22 @@ public class GameController : MonoBehaviour
         }
         }
     
+    public void Shuffle(List<string> a)
+    {
+        // Loop array
+        for (int i = a.Count - 1; i > 0; i--)
+        {
+            // Randomize a number between 0 and i (so that the range decreases each time)
+            int rnd = UnityEngine.Random.Range(0, i);
+
+            // Save the value of the current i, otherwise it'll overwrite when we swap the values
+            string temp = a[i];
+
+            // Swap the new and old values
+            a[i] = a[rnd];
+            a[rnd] = temp;
+        }
+
+
+    }    
 }
